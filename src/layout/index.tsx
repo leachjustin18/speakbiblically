ed03e46bde2aabfb48a0de3a4d503d7462c23e2b
leachjustin18 <leachjustin18@yahoo.com>
2019-03-30 20:15:19 -0500

    Added helmet to layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 interface LayoutProps {
   children: JSX.Element[];
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => <main>{children}</main>;
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <main>
+    <Helmet>
+      <title>Speak Biblically</title>
+      <meta
+        name="description"
+        content="Welcome to Speak Biblically!  On this site we are concerned about God's truth"
+      />
+    </Helmet>
+
+    {children}
+  </main>
+);
 
 export default Layout;
