9f608402ae9059cdce60d812e776ebd745eeeb4b
leachjustin18 <leachjustin18@yahoo.com>
2019-03-30 20:22:42 -0500

    Allow layout to accept both jsx element array and single

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 interface LayoutProps {
-  children: JSX.Element[];
+  children: JSX.Element[] | JSX.Element;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
