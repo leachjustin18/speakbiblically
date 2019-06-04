96116c4bef2bbac6255c8c45e05109961a29c52d
leachjustin18 <leachjustin18@yahoo.com>
2019-06-03 21:10:50 -0500

    Added Bible bkround to layout and added styles to it.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,32 @@
 import React, { FC } from 'react';
 import Helmet from 'react-helmet';
+import { withStyles } from '@material-ui/core/styles';
+import { WithStyles } from '@material-ui/core';
 import withRoot from '../withRoot';
 import Header from '../components/Header';
+import Bible from '../images/Bible.jpg';
 
-interface LayoutProps {
+interface LayoutProps extends WithStyles<typeof styles> {
   children: JSX.Element[] | JSX.Element;
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => (
+const styles = () => ({
+  backGroundImage: {},
+  '@media (min-width: 1025px)': {
+    backGroundImage: {
+      opacity: 0.5,
+      top: 0,
+      bottom: 0,
+      width: '100%',
+      position: 'fixed' as 'fixed',
+      zIndex: -1,
+      backgroundImage: `url(${Bible})`,
+      backgroundSize: 'cover',
+    },
+  },
+});
+
+const Layout: FC<LayoutProps> = ({ classes, children }) => (
   <main>
     <Helmet>
       <html lang="en" />
@@ -21,7 +40,9 @@ const Layout: FC<LayoutProps> = ({ children }) => (
     <Header />
 
     {children}
+
+    <div className={classes.backGroundImage} />
   </main>
 );
 
-export default withRoot(Layout);
+export default withRoot(withStyles(styles)(Layout));
