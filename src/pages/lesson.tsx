2585fb49430c3e4b94df2a84fc2a5623cd9e873a
leachjustin18 <leachjustin18@yahoo.com>
2019-06-11 20:14:57 -0500

    Working on lesson page.

--- src/pages/lesson.tsx
+++ src/pages/lesson.tsx
@@ -1,7 +1,24 @@
 import React, { FC } from 'react';
-
+import Typography from '@material-ui/core/Typography';
+import YouTube from 'react-youtube';
 import Layout from '../layout';
 
-const Lesson: FC = () => <Layout>Lesson</Layout>;
+const youTubeOptions: { playerVars: { rel: number } } = {
+  playerVars: {
+    rel: 0,
+  },
+};
+
+const Lesson: FC = () => (
+  <Layout>
+    <section>
+      <Typography component="h3" variant="h3" gutterBottom={true}>
+        Does my church teach the truth?
+      </Typography>
+
+      <YouTube videoId="GY0Bdt9nLT8" opts={youTubeOptions} />
+    </section>
+  </Layout>
+);
 
 export default Lesson;
