9c1cf5b8b13d2b91bfad20e52b4d6195a0a7c74f
Justin Leach <leachjustin18@yahoo.com>
2023-09-26 18:54:23 -0500

    Fix netlify issue (#75)
    * Remove window from date
    
    * Working on build issues

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,17 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 const path = require(`path`);
 
 const config: GatsbyConfig = {
+  flags: {
+    DEV_SSR: true,
+  },
   siteMetadata: {
     title: `SpeakBiblicallyUpdates`,
-    siteUrl: "https://www.speakbiblically.com/",
+    siteUrl: 'https://www.speakbiblically.com/',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -16,34 +19,34 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
         spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
         downloadLocal: true,
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: path.join(__dirname, "src", "images"),
+        name: 'images',
+        path: path.join(__dirname, 'src', 'images'),
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/favicon.png",
+        icon: 'src/images/favicon.png',
       },
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-robots-txt",
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-robots-txt',
   ],
 };
 
