5e4a0eb45eeb89367897b03879d40bd67f415f9f
Justin Leach <leachjustin18@yahoo.com>
2023-12-27 12:36:52 -0600

    Have footer always be at the bottom

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
 import MUIThemeProvider from './MUIRootWrapper';
 import Header from './src/components/Header';
 import Footer from './src/components/Footer';
@@ -8,13 +10,23 @@ export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
   props,
   element,
 }) => {
+  const Container = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  `;
+
+  const Main = styled(Box)`
+    flex: 1;
+  `;
+
   return (
     <MUIThemeProvider {...props}>
-      <main>
+      <Container>
         <Header />
-        {element}
+        <Main component="main">{element}</Main>
         <Footer />
-      </main>
+      </Container>
     </MUIThemeProvider>
   );
 };
