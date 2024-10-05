e63df83b4a00cc6a62c5e69aa29d7228abd9460d
Justin Leach <leachjustin18@yahoo.com>
2024-10-05 14:22:22 -0500

    Update and fix x link and add TIkTok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ComponentClass } from 'react';
 import styled from '@emotion/styled';
 import { grey } from '@mui/material/colors';
 import { Box, Button, Stack } from '@mui/material';
@@ -10,15 +10,28 @@ import {
   Pinterest,
   Instagram,
   YouTube,
+  X,
 } from '@mui/icons-material';
 
 const Footer = () => {
-  const FooterButton = styled(Button)<any>`
+  const FooterButton: ComponentClass<any> = styled(Button)`
     color: #d9a762;
     &:hover {
       color: #f2d2b7;
     }
   `;
+
+  const TikTokIcon: ComponentClass<any> = () => (
+    <svg
+      fill="#d9a762"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      width="15px"
+      height="15px"
+    >
+      <path d="M41,4H9C6.243,4,4,6.243,4,9v32c0,2.757,2.243,5,5,5h32c2.757,0,5-2.243,5-5V9C46,6.243,43.757,4,41,4z M37.006,22.323 c-0.227,0.021-0.457,0.035-0.69,0.035c-2.623,0-4.928-1.349-6.269-3.388c0,5.349,0,11.435,0,11.537c0,4.709-3.818,8.527-8.527,8.527 s-8.527-3.818-8.527-8.527s3.818-8.527,8.527-8.527c0.178,0,0.352,0.016,0.527,0.027v4.202c-0.175-0.021-0.347-0.053-0.527-0.053 c-2.404,0-4.352,1.948-4.352,4.352s1.948,4.352,4.352,4.352s4.527-1.894,4.527-4.298c0-0.095,0.042-19.594,0.042-19.594h4.016 c0.378,3.591,3.277,6.425,6.901,6.685V22.323z" />
+    </svg>
+  );
 
   return (
     <Box component="footer" sx={{ backgroundColor: grey[900] }} py={3}>
@@ -81,12 +94,23 @@ const Footer = () => {
       >
         <Box>
           <FooterButton
-            href="https://twitter.com/39COFC"
+            href="https://www.tiktok.com/@39thstreetcofc"
             target="blank"
             rel="nofollow"
-            startIcon={<Twitter />}
+            startIcon={<TikTokIcon />}
           >
-            Twitter (X)
+            TikTok
+          </FooterButton>
+        </Box>
+
+        <Box>
+          <FooterButton
+            href="https://x.com/39streetcofc"
+            target="blank"
+            rel="nofollow"
+            startIcon={<X />}
+          >
+            X
           </FooterButton>
         </Box>
 
